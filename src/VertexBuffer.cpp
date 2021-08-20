8bc7b600ca1fe374bf6110f45d5cd5140685122f
nathanstephenson <nathans261@gmail.com>
2021-08-20 14:01:09 +0100

    abstraction of vertex buffer

---
+++ src/VertexBuffer.cpp
@@ -0,0 +1,20 @@
+#include "VertexBuffer.h"
+
+VertexBuffer::VertexBuffer(const void* data, unsigned int size){
+    //Create and bind vertex buffer, then feed it data
+    GLCall(glGenBuffers(1, &m_RendererID));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));//add vertex data
+}
+
+VertexBuffer::~VertexBuffer(){
+    GLCall(glDeleteBuffers(1, &m_RendererID));
+}
+
+void VertexBuffer::Bind() const{
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+}
+
+void VertexBuffer::Unbind() const{
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+}
---
+++ src/VertexBuffer.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Renderer.h"
+
+#include <GL/glew.h>
+
+class VertexBuffer {
+private:
+	unsigned int m_RendererID;//unique id for the specific vb object being created
+public:
+	VertexBuffer(const void* data, unsigned int size);
+	~VertexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+};
