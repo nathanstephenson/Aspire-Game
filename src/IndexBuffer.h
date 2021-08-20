d19fa4cd11d770cd8c0e1b3a482b758f9af0f58b
nathanstephenson <nathans261@gmail.com>
2021-08-20 14:01:00 +0100

    abstraction of index buffer

---
+++ src/IndexBuffer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Renderer.h"
+
+#include <GL/glew.h>
+
+class IndexBuffer {
+private:
+	unsigned int m_RendererID;//unique id for the specific vb object being created
+	unsigned int m_Count;
+public:
+	IndexBuffer(const unsigned int* data, unsigned int count);
+	~IndexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+
+	inline unsigned int GetCount() const { return m_Count; }
+};
