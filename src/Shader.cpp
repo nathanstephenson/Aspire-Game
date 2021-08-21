4c9b65bd3d24ba8a59d400c6f4aad0ef25acf9f9
nathanstephenson <nathans261@gmail.com>
2021-08-21 14:42:56 +0100

    removed logging of shader code

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -9,8 +9,8 @@
 Shader::Shader(const std::string& filepath) : m_FilePath(filepath), m_RendererID(0){
     ShaderSource shaders = ParseShader(filepath);
     m_RendererID = CreateShader(shaders.vertexSource, shaders.fragmentSource);//create shader from sources
-    std::cout << "VERTEX" << std::endl << shaders.vertexSource;
-    std::cout << "FRAGMENT" << std::endl << shaders.fragmentSource;
+    //std::cout << "VERTEX" << std::endl << shaders.vertexSource;
+    //std::cout << "FRAGMENT" << std::endl << shaders.fragmentSource;
     
     
 }
