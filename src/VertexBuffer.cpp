9b7de9381ad0c9121660ac7f87abfe04818a081a
nathanstephenson <nathans261@gmail.com>
2021-08-26 01:47:02 +0100

    Trying to implement dynamic geometry
    Nothing is being rendered at all when using a dynamic vertex buffer. Really unsure why.

--- src/VertexBuffer.cpp
+++ src/VertexBuffer.cpp
@@ -1,11 +1,16 @@
 //#include "VertexBuffer.h"
 #include "Renderer.h"
+#include <iostream>
 
 VertexBuffer::VertexBuffer(const void* data, unsigned int size){
-    //Create and bind vertex buffer, then feed it data
+    //Create and bind vertex buffer, then feed it data if static
     GLCall(glGenBuffers(1, &m_RendererID));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));//add vertex data
+    if (data != nullptr) {
+        GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));//add vertex data
+    } else {
+        GLCall(glBufferData(GL_ARRAY_BUFFER, size, nullptr, GL_DYNAMIC_DRAW));//add vertex data
+    }
 }
 
 VertexBuffer::~VertexBuffer(){
@@ -18,4 +23,10 @@ void VertexBuffer::Bind() const{
 
 void VertexBuffer::Unbind() const{
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+}
+
+void VertexBuffer::UpdateData(const void* data) const{
+    Bind();
+    GLCall(glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(data), data));
+    //std::cout << "Updated Dynamic VBO Buffer data, size:" << sizeof(data) << std::endl;
 }
