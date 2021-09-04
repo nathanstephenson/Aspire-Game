489d0ccb7fe511ceba48ab34ad3952aef27ab4c9
nathanstephenson <nathans261@gmail.com>
2021-09-04 14:17:38 +0100

    cleaned up some "includes" from main

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,6 @@
 #include <imgui/imgui_impl_glfw_gl3.h>
 
 #include "Renderer.h"
-#include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
-#include "IndexBuffer.h"
-#include "VertexArray.h"
-#include "Shader.h"
-#include "Texture.h"
 #include "EventHandler.h"
 #include "Application.h"
 
