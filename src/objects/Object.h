12f82d1cb0777678bc44d186d604a2cadf8b5e25
nathanstephenson <nathans261@gmail.com>
2021-09-05 23:43:26 +0100

    expanded object subclass

--- src/objects/Object.h
+++ src/objects/Object.h
@@ -1,32 +1,20 @@
 #pragma once
-#include <string>
-#include <vector>
-#include <iostream>
-#include <functional>
+#include "Renderer.h"
 
 class Object {
 private:
-
+	vec2 m_Size;
+	vec2 m_Origin;
 public:
-	Object() {}
+	Object() : m_Size({ 0.0, 0.0 }), m_Origin({ 0.0, 0.0 }) {}
 	virtual ~Object() {}
 
 	virtual void OnUpdate(float deltaTime) {}
 	virtual void OnRender() {}
 	virtual void OnImGuiRender() {}
-};
-class ObjectContainer : public Object {
-private:
-	std::vector <Object*> m_Objects;
-public:
-	ObjectContainer(Object*& currentTestPointer) {}
 
-	void OnImGuiRender() override;
-
-	std::vector <Object*> GetObjects() { return m_Objects; }
-
-	void AddObject(Object* obj) {
-		m_Objects.push_back(obj);
-	}
-
+	virtual void SetDimensions(float width, float height) { m_Size = {width, height}; }
+	vec2 GetDimensions() { return m_Size; }
+	virtual void SetOrigin(float x, float y) { m_Origin = {x, y}; }
+	vec2 GetOrigin() { return m_Origin; }
 };
