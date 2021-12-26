5d9cd3e67e97495acaa891ed904b74227b19d5ea
nathanstephenson <nathans261@gmail.com>
2021-12-26 20:35:21 +0000

    separated object class into header and cpp

---
+++ src/objects/Object.cpp
@@ -0,0 +1,40 @@
+#include "Object.h"
+
+Object::Object() : m_VertexPosition(0), m_Size({ 0.0, 0.0, 0.0 }), m_Origin({ 0.0, 0.0, 0.0 }), m_Rotation(0), m_Vertices({}), m_Indices({}), m_Type(blank) {
+
+}
+
+void Object::OnUpdate() {}
+void Object::OnRender() {}
+void Object::OnImGuiRender() {}
+
+void Object::SetVertexPosition(unsigned int i) { 
+	m_VertexPosition = i; 
+}
+unsigned int Object::GetVertexPosition() { return m_VertexPosition; }
+
+void Object::SetDimensions(float width, float height) { 
+	m_Size = { width, height }; 
+}
+vec3 Object::GetDimensions() { return m_Size; }
+
+void Object::SetOrigin(float x, float y, float z) {
+	m_Origin = { x, y };
+	OnUpdate();
+}
+vec3 Object::GetOrigin() { return m_Origin; }
+
+void Object::SetVertices(std::vector<Vertex> vertices) { 
+	m_Vertices = vertices;/*memcpy(m_Vertices.data(), vertices.data(), m_Vertices.size());*/ 
+}
+Vertex* Object::GetVertices() { return m_Vertices.data(); }
+
+void Object::SetIndices(std::vector<unsigned int> indices) { 
+	m_Indices = indices; 
+}
+unsigned int* Object::GetIndices() { //allows indices to be manipulated based on the object's starting vertex position in the vertexbuffer
+	std::valarray<unsigned int> ind(m_Indices.data(), m_Indices.size());
+	ind += m_VertexPosition;
+	std::vector<unsigned int>indices(std::begin(ind), std::end(ind));
+	return m_Indices.data();
+}
--- src/objects/Object.h
+++ src/objects/Object.h
@@ -13,12 +13,12 @@ private:
 	std::vector<unsigned int> m_Indices;
 	ObjectType m_Type;
 public:
-	Object() : m_VertexPosition(0), m_Size({ 0.0, 0.0, 0.0 }), m_Origin({ 0.0, 0.0, 0.0 }), m_Rotation(0), m_Vertices({}), m_Indices({}), m_Type(blank) {}
-	virtual ~Object() {}
+	Object();
+	virtual ~Object();
 
-	virtual void OnUpdate() {}
-	virtual void OnRender() {}
-	virtual void OnImGuiRender() {}
+	virtual void OnUpdate();
+	virtual void OnRender();
+	virtual void OnImGuiRender();
 
 	/*void Transform(float x, float y, float z, float rotation) {
 		std::vector<Vertex> v = *GetVertices();		
@@ -31,26 +31,18 @@ public:
 		SetOrigin(v[0].Position.x, v[0].Position.y);
 	}*/
 
-	void SetVertexPosition(unsigned int i) { m_VertexPosition = i; }
-	unsigned int GetVertexPosition() { return m_VertexPosition; }
+	void SetVertexPosition(unsigned int i);
+	unsigned int GetVertexPosition();
 
-	void SetDimensions(float width, float height) { m_Size = {width, height}; }
-	vec3 GetDimensions() { return m_Size; }
+	void SetDimensions(float width, float height);
+	vec3 GetDimensions();
 
-	void SetOrigin(float x, float y, float z) { 
-		m_Origin = {x, y};
-		OnUpdate();
-	}
-	vec3 GetOrigin() { return m_Origin; }
+	void SetOrigin(float x, float y, float z);
+	vec3 GetOrigin();
 
-	void SetVertices(std::vector<Vertex> vertices) { m_Vertices = vertices;/*memcpy(m_Vertices.data(), vertices.data(), m_Vertices.size());*/ }
-	Vertex* GetVertices() { return m_Vertices.data(); }
+	void SetVertices(std::vector<Vertex> vertices);
+	Vertex* GetVertices();
 
-	void SetIndices(std::vector<unsigned int> indices) { m_Indices = indices; }
-	unsigned int* GetIndices() { //allows indices to be manipulated based on the object's starting vertex position in the vertexbuffer
-		std::valarray<unsigned int> ind(m_Indices.data(), m_Indices.size());
-		ind += m_VertexPosition;
-		std::vector<unsigned int>indices(std::begin(ind), std::end(ind));
-		return m_Indices.data();
-	}
+	void SetIndices(std::vector<unsigned int> indices);
+	unsigned int* GetIndices();
 };
