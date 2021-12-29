c7cbb9b84f3848e5ce04c31541bfc18114741666
nathanstephenson <nathans261@gmail.com>
2021-12-29 00:21:16 +0000

    Update BufferController.cpp

--- src/BufferController.cpp
+++ src/BufferController.cpp
@@ -6,7 +6,7 @@ BufferController::BufferController() : m_Vertices(), m_Indices() {
     m_Shader = std::make_unique<Shader>("res/shaders/shader.shader");
     m_IndexBuffer = std::make_unique<IndexBuffer>(nullptr, 1500 * sizeof(unsigned int));//can hold up to 1500 indices to be mapped to vertices
     VertexBufferLayout layout;
-    layout.Push<float>(2);//setting up the layout of the vertices array with its type and stride
+    layout.Push<float>(3);//setting up the layout of the vertices array with its type and stride
     layout.Push<float>(4);//repeated to include rgba values
     layout.Push<float>(2);//repeated to include texture coordinates
     layout.Push<float>(1);//repeated to include texture index
@@ -23,8 +23,10 @@ void BufferController::RemoveObject() {
 }
 
 void BufferController::UpdateObject(Vertex* vertices, unsigned int vcount, unsigned int* indices, unsigned int icount) {
+    BindBuffers();
     SetVertices(vertices, vcount);
     SetIndices(indices, icount);
+    UnbindBuffers();
 }
 
 void BufferController::BindBuffers() {
@@ -42,24 +44,36 @@ void BufferController::UnbindBuffers() {
 }
 
 void BufferController::BindTextureSlots(unsigned int start, unsigned int end) {
+    m_Shader->Bind();
     for (unsigned int i = start; i <= end; i++) {
         m_Shader->SetUniform1i("u_Textures[" + std::to_string(i) + "]", i);//may require "#include <string>"
     }
+    m_Shader->Unbind();
 }
 
 void BufferController::Draw(glm::mat4 mvp) {
+    BindBuffers();
     m_Shader->SetUniformMat4f("u_MVP", mvp);
     glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
+    UnbindBuffers();
 }
 
 void BufferController::SetVertices(Vertex* vertices, unsigned int count) {
-    memcpy(m_Vertices, vertices, count * sizeof(Vertex));
-    //std::copy(vertices[0], vertices[3], m_Vertices);
+    BindBuffers();
+    //memcpy(m_Vertices, vertices, count * sizeof(Vertex));
+    for (unsigned int i = 0; i < count; i++) {
+        m_Vertices[i] = vertices[i];
+    }
     m_VertexBuffer->UpdateData(m_Vertices, count * sizeof(Vertex));
+    UnbindBuffers();
 }
 
 void BufferController::SetIndices(unsigned int* indices, unsigned int count) {
-    memcpy(m_Indices, indices, count * sizeof(unsigned int));
-    //std::copy(indices[0], indices[11], m_Indices);
+    BindBuffers();
+    //memcpy(m_Indices, indices, count * sizeof(unsigned int));
+    for (unsigned int i = 0; i < count; i++) {
+        m_Indices[i] = indices[i];
+    }
     m_IndexBuffer->UpdateData(m_Indices, count * sizeof(unsigned int));
+    UnbindBuffers();
 }
